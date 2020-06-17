bbe0e0588aa8eda0ead122b4534d5f26756feac4
i432855 <432855@student.fontys.nl>
2020-06-17 17:06:12 +0200

    Basic layout

---
+++ SprintX.js
@@ -0,0 +1,8 @@
+function calculate(elem){
+    if (elem.style.backgroundColor == "green") {
+        elem.style.backgroundColor = "#EBF2F0"
+    }
+    else {
+    elem.style.backgroundColor = "green";
+    }
+}
